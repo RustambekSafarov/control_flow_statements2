36a8bea39730e0b5e2400ff8ef84c33b9229f1e7
Rustambek <rustambeksafarovv@gmail.com>
2022-06-30 22:35:05 +0500

    Redone

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,20 @@ Args:
 Returns:
     int: return answer.
 */
+int func(a,b,c){
+    if (a>b){
+        if(a>c){
+            return a;
+        } else{
+            return c;
+        }
+    }
+    if (b>c){
+        return b;
+    } else{
+        return c;
+    }
+}
 void main() {
+    print(func(4,6,3));
 }
