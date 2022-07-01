a5962259f87e52d3eaeaf7ee671eab94c3f9e442
Rustambek <rustambeksafarovv@gmail.com>
2022-07-01 08:54:06 +0500

    Redone

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,18 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+int func(a,b,c){
+    if (a<b){
+        if (b<c){
+            return b;
+        }
+        return c;
+    }
+    if (a<c){
+        return a;
+    }
+    return c;
+}
 void main() {
+    print(func(4,6,9));
 }
